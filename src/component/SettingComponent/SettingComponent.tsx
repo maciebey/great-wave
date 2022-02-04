215dfac3e6277d79b59d2c8e533c107b2ad3fab5
Macie Bey <maciebey@gmail.com>
2022-02-04 16:20:47 -0600

    Added display of current values of opacity/color on setting component

--- src/component/SettingComponent/SettingComponent.tsx
+++ src/component/SettingComponent/SettingComponent.tsx
@@ -20,23 +20,30 @@ const SettingComponent = ({ layer, onChange }: Props) => {
   };
 
   return (
-    <>
-      <div>{layer.name}</div>
-      <input
-        id="typeinp"
-        type="range"
-        min="0" max="1"
-        value={layer.opacity}
-        onChange={(event) => opacityChange(event)}
-        step=".01"
-      />
-      <select value={layer.color} onChange={(event) => colorChange(event)}>
-        <option value="#6166fb">#6166fb</option>
-        <option value="#fb6161">#fb6161</option>
-        <option value="#3cda4e">#3cda4e</option>
-        <option value="#c800ff">#c800ff</option>
-      </select>
-    </>
+    <div>
+      <div>Layer: {layer.name}</div>
+      <div className='opacity-control'>
+        <p>Opacity: {layer.opacity} </p>
+        <input
+          id="typeinp"
+          type="range"
+          min="0" max="1"
+          value={layer.opacity}
+          onChange={(event) => opacityChange(event)}
+          step=".01"
+        />
+      </div>
+      <div className='color-control'>
+        <p>Color:</p>
+        <div className='color-display' style={{ backgroundColor: layer.color }}></div>
+        <select value={layer.color} onChange={(event) => colorChange(event)}>
+          <option value="#6166fb">#6166fb</option>
+          <option value="#fb6161">#fb6161</option>
+          <option value="#3cda4e">#3cda4e</option>
+          <option value="#c800ff">#c800ff</option>
+        </select>
+      </div>
+    </div>
   )
 }
 
