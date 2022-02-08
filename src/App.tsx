c1afec7259ac6d53056d7852f502736a37982739
Macie Bey <maciebey@gmail.com>
2022-02-08 14:59:10 -0600

    Updates the app in the following ways: -added html-to-image dependency -implemented capture of curent canvas and display in modal -implemented basic save image to file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header, Footer, SvgComponent, SettingComponent } from './component';
 import { layer } from './interfaces';
+import * as htmlToImage from 'html-to-image';
 import './App.css';
 
 import { useState, useEffect } from 'react';
@@ -17,6 +18,11 @@ function App(this: any) {
   const [images, setImages] = useState<layer[]>([]);
 
   const [isLoading, setIsLoading] = useState(true);
+
+  const [modalDisplayClass, setModalDisplayClass] = useState('hide');
+
+  let canvasRef: HTMLElement;
+  let renderRef: HTMLElement;
 
   useEffect(() => {
     // will only load data once on page load
@@ -51,18 +57,65 @@ function App(this: any) {
     newData[index] = layer;
     setImages(newData);
   };
+
+  const closeModal = () => {
+    setModalDisplayClass('hide');
+  }
+
+  /* html-to-image guide
+  * https://betterprogramming.pub/heres-why-i-m-replacing-html2canvas-with-html-to-image-in-our-react-app-d8da0b85eadf
+  */
+  const captureCanvas = () => {
+    if (canvasRef && renderRef) {
+      htmlToImage.toCanvas(canvasRef).then(function(canvas) {
+        renderRef.replaceChildren(canvas);
+      });
+    }
+    setModalDisplayClass('');
+    
+  };
+
+  const saveCanvas = () => {
+    if (canvasRef) {
+      htmlToImage.toPng(canvasRef).then(function (dataUrl) {
+        saveAs(dataUrl, 'my-node.png');
+      });
+    }
+  }
+
+  const saveAs = (blob: any, fileName: any) =>{
+    var elem = window.document.createElement('a');
+    elem.href = blob
+    elem.download = fileName;
+    // elem.style = 'display:none;';
+    (document.body || document.documentElement).appendChild(elem);
+    if (typeof elem.click === 'function') {
+      elem.click();
+    } else {
+      elem.target = '_blank';
+      elem.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+      }));
+    }
+    URL.revokeObjectURL(elem.href);
+    elem.remove()
+  }
   
   return (
     <div className="App">
       <Header></Header>
       <main>
-        <div className="img-container">
-          {images.length && images.map((i, index) => (
-            <SvgComponent layer={i} key={index} />
-          ))}
-          {/* {images.length && images.map((i) => (
-            <img key={i.name} alt={i.name} src={`${iconPath}${i.file}`} style={{opacity: i.opacity}} />
-          ))} */}
+        <div id="canvas">
+          <div ref={node => {if (node) canvasRef = node}}  className="img-container">
+            {images.length && images.map((i, index) => (
+              <SvgComponent layer={i} key={index} />
+            ))}
+            {/* {images.length && images.map((i) => (
+              <img key={i.name} alt={i.name} src={`${iconPath}${i.file}`} style={{opacity: i.opacity}} />
+            ))} */}
+          </div>
         </div>
         <div className="control-container">
           {images.length && images.map((i, index) => (
@@ -73,8 +126,16 @@ function App(this: any) {
             />
           ))}
         </div>
+        <div onClick={captureCanvas} className="capture-button button">Capture</div>
       </main>
       <Footer></Footer>
+      <div className={'modal ' + modalDisplayClass}>
+        <div className='modal-content'>
+          <div onClick={closeModal} className="close-button button">X</div>
+          <div ref={node => {if (node) renderRef = node}} id="append"></div>
+          <div onClick={saveCanvas} className="save-button button">Save As Png</div>
+        </div>
+      </div>
     </div>
   );
 }
